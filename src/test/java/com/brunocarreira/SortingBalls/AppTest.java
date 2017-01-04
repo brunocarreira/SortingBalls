e0c580d72028d7dd0d63bc6d7bec8e10cbf7e630
brunocarreira <brunocarreira@gmail.com>
2017-01-04 11:12:26 +0100

    JUnit test

---
+++ src/test/java/com/brunocarreira/SortingBalls/AppTest.java
@@ -0,0 +1,27 @@
+package com.brunocarreira.SortingBalls;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Test the insertion in correct order
+     * The range test (0-60) is not made because it's not a domain requirement
+     */
+    public void testInsertionBalls()
+    {
+    	Rack rack = new Rack();
+    	rack.add(new Ball(10));
+    	assertEquals("10", rack.toString());
+    	
+    	rack.add(new Ball(30));
+    	assertEquals("10,30", rack.toString());
+
+    	rack.add(new Ball(20));
+    	assertEquals("10,20,30", rack.toString());
+    }
+}
